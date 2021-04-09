799f6de2b906809605a267e41488b66534d77f3d
juju <gliheng@live.com>
2021-04-09 21:56:39 +0800

    dev CustomLayout

---
+++ example/src/views/CustomLayoutLibrary.tsx
@@ -0,0 +1,62 @@
+export default {
+  a: {
+    render() {
+      return <div style={{
+        backgroundColor: 'purple',
+        color: 'white',
+        fontSize: '3em',
+        width: '100%',
+        height: '100%',
+        display: 'flow-root',
+      }}><span>A</span></div>;
+    },
+  },
+  b: {
+    render() {
+      return <div style={{
+        backgroundColor: 'crimson',
+        color: 'white',
+        fontSize: '3em',
+        width: '100%',
+        height: '100%',
+        display: 'flow-root',
+      }}><span>B</span></div>;
+    },
+  },
+  c: {
+    render() {
+      return <div style={{
+        backgroundColor: 'blue',
+        color: 'white',
+        fontSize: '3em',
+        width: '100%',
+        height: '100%',
+        display: 'flow-root',
+      }}><span>C</span></div>;
+    },
+  },
+  d: {
+    render() {
+      return <div style={{
+        backgroundColor: 'navy',
+        color: 'white',
+        fontSize: '3em',
+        width: '100%',
+        height: '100%',
+        display: 'flow-root',
+      }}><span>D</span></div>;
+    },
+  },
+  e: {
+    render() {
+      return <div style={{
+        backgroundColor: 'lime',
+        color: 'white',
+        fontSize: '3em',
+        width: '100%',
+        height: '100%',
+        display: 'flow-root',
+      }}><span>E</span></div>;
+    },
+  },
+};
