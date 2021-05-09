2a83f759526644e5d030637692c15d1e843c4513
juju <gliheng@live.com>
2021-05-09 10:27:20 +0800

    export css in seperate files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { dest, src } = require("gulp");
+const { sass } = require("@mr-hope/gulp-sass");
+const rename = require('gulp-rename');
+
+const buildCss = function() {
+  return src([
+    './src/components/**/*.scss',
+    './src/assets/styles/index.scss',
+  ]).pipe(sass({
+    includePaths: ['./src/assets/styles']
+  }).on("error", sass.logError))
+  .pipe(rename({dirname: ''}))
+  .pipe(dest("./dist/styles"));
+}
+
+exports.buildCss = buildCss;
