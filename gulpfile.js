fbb3721d9fef84c06c3d960e913712b22582cd94
juju <gliheng@live.com>
2021-12-07 13:50:03 +0800

    fix css build

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const rename = require('gulp-rename');
 
 const buildCss = function() {
   return src([
-    './src/components/**/*.scss',
+    './src/**/*.scss',
     './src/assets/styles/index.scss',
   ]).pipe(sass({
     includePaths: ['./src/assets/styles']
