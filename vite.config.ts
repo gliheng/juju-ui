be2d2f45acc2b13d29f4517a66b618080e9fb762
juju <gliheng@live.com>
2021-12-07 13:50:38 +0800

    vite build

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ['./src/assets/styles']
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@directives": "/src/directives",
+      "@utils": "/src/utils" 
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'juju-ui'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        globals: {
+          vue: 'Vue',
+        }
+      }
+    }
+  }
+});
