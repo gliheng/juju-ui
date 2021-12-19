d8059ed959a95c05df13b658995e3f096df0a4c0
juju <gliheng@live.com>
2021-12-19 22:32:14 +0800

    fix sass build

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 const { dest, src } = require("gulp");
-const { sass } = require("@mr-hope/gulp-sass");
+const sass = require("sass");
+const through = require("through2");
 const rename = require('gulp-rename');
+
+const { Transform } = require('stream');
+
+const sassTransform = new Transform({
+  objectMode: true,
+  async transform(obj, encoding, callback) {
+    let ret = await sass.compileStringAsync(
+      obj.contents.toString(encoding),
+      {
+        loadPaths: ['src/assets/styles'],
+      },
+    );
+    obj.contents = Buffer.from(ret.css, encoding);
+    this.push(obj);
+    callback();
+  }
+});
 
 const buildCss = function() {
   return src([
     './src/**/*.scss',
     './src/assets/styles/index.scss',
-  ]).pipe(sass({
-    includePaths: ['./src/assets/styles']
-  }).on("error", sass.logError))
+  ])
+  .pipe(sassTransform)
   .pipe(rename({dirname: ''}))
   .pipe(dest("./dist/styles"));
 }
