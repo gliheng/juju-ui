34d4ac82b06bc9b5318f4508a16af1f1f696fc91
juju <gliheng@live.com>
2022-04-30 18:17:09 +0800

    fix button size

--- example/src/views/Button/parts/VariantPart.vue
+++ example/src/views/Button/parts/VariantPart.vue
@@ -2,5 +2,5 @@
   <j-button outlined>Outlined</j-button>
   <j-button flat>Flat</j-button>
   <j-button raised>Raised</j-button>
-  <j-button rounded>Rounded</j-button>
+  <j-button round>Round</j-button>
 </template>
